5f9ca6cfbc3d79ec24b0b3e8d2b257ccbdedd94b
ChiawNa <chiawnak0390@gmail.com>
2025-02-24 01:22:51 +0800

    update

--- C1 Runnig_YOLOv8_on_Windows/prediction.py
+++ C1 Runnig_YOLOv8_on_Windows/prediction.py
@@ -5,7 +5,7 @@ model = YOLO("yolov8n.pt")
 
 ##Predict Method Takes all the parameters of the Command Line Interface
 
-model.predict(source='demo.mp4', save=True, conf=0.5, save_txt=True)
+model.predict(source='2.png', save=True, conf=0.5, save_txt=True)
 # model.export(format="onnx")
 
 
